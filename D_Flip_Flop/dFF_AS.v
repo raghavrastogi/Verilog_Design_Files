45401a902264242ce174a14b0a3b2287ec7b051d
Raghav Rastogi <11006710+raghavrastogi@users.noreply.github.com>
2018-08-28 22:27:43 +0530

    Add files via upload

---
+++ D_Flip_Flop/dFF_AS.v
@@ -0,0 +1,34 @@
+//------------------------------------------------
+// D Flip Flop with synchronous reset.
+//------------------------------------------------
+//  Functionality:
+//                Q Output data of flip flop.
+//                QB inverted output of Q.
+//                data data input.
+//                clk clock input.
+//                rst reset input.
+//                If reset is enabled the flip flop will reset independent of the clock (Asynchronous).
+//------------------------------------------------
+//  Design Architecture = Behavioural Architecture
+//------------------------------------------------
+//  Module Name   : dFF;
+//  Designer      : Raghav Rastogi;
+//  Creation Date : 28, August, 2018;
+//------------------------------------------------
+
+module dFF (Q, QB, data, clk, rst);
+  
+	output Q, QB;
+	input data, clk, rst;
+	reg Q;
+  
+	assign QB = ~Q;
+  
+	always @(posedge clk, posedge rst)
+	begin
+		if(rst)
+			Q <= 1'b0;
+		else
+			Q <= data;
+	end
+endmodule
---
+++ D_Flip_Flop/dFF_AS_tb.v
@@ -0,0 +1,42 @@
+module dFF_tb;
+  
+	reg data, clk, rst;
+	wire Q, QB;
+  
+	dFF DUT (	.Q(Q),
+				.QB(QB),
+				.data(data),
+				.clk(clk),
+				.rst(rst)	);
+  
+	initial begin
+		clk = 0;
+		rst = 1;
+		forever begin
+			#5 clk = ~clk;
+		end
+	end
+  
+	initial begin
+		#14
+		rst = 0;
+		#14
+		rst = 1;
+		#14
+		rst = 0;
+	end
+  
+	initial begin
+		#12
+		data = 0;
+		#10
+		data = 1;
+		#30
+		data = 0;
+	end
+  
+	initial begin
+		$monitor($time, "CLK = %b , RST = %b , DATA = %b", clk, rst, data);
+	end
+  
+endmodule
