f84670e1d58a0f9e97d0bb593b9f23ad0b4c5055
Raghav Rastogi <raghavrastogi08@yahoo.co.in>
2018-08-28 23:39:26 +0530

    Update dFF.v

--- D_Flip_Flop/Synchronous/Design/dFF.v
+++ D_Flip_Flop/Synchronous/Design/dFF.v
@@ -1,21 +1,23 @@
-//------------------------------------------------
-// D Flip Flop with synchronous reset.
-//------------------------------------------------
-//  Functionality:
-//                Q Output data of flip flop.
-//                QB inverted output of Q.
-//                data data input.
-//                clk clock input.
-//                rst reset input.
-//                If reset is enabled after the rising edge of the clock and remains high until next rising edge only then the D Flip Flop
-//                will reset(Synchronous Reset).
-//------------------------------------------------
-//  Design Architecture = Behavioural Architecture
-//------------------------------------------------
-//  Module Name   : dFF;
-//  Designer      : Raghav Rastogi;
-//  Creation Date : 28, August, 2018;
-//------------------------------------------------
+/*
+------------------------------------------------
+D Flip Flop with synchronous reset.
+------------------------------------------------
+Functionality:
+                Q Output data of flip flop.
+                QB inverted output of Q.
+                data data input.
+                clk clock input.
+                rst reset input.
+                If reset is enabled after the rising edge of the clock and remains high until next rising edge only then the D Flip Flop
+                will reset(Synchronous Reset).
+------------------------------------------------
+Design Architecture = Behavioural Architecture
+------------------------------------------------
+Module Name   : dFF;
+Designer      : Raghav Rastogi;
+Creation Date : 28, August, 2018;
+------------------------------------------------
+*/
 
 module dFF (Q, QB, data, clk, rst);
   
