7a5cd7f5a6b660aaf0e4b82b2b38b1762cb8209a
Raghav Rastogi <raghavrastogi08@yahoo.co.in>
2018-08-28 23:37:35 +0530

    Update dFF_AS.v

--- D_Flip_Flop/Asynchronous/dFF_AS.v
+++ D_Flip_Flop/Asynchronous/dFF_AS.v
@@ -1,20 +1,22 @@
-//------------------------------------------------
-// D Flip Flop with synchronous reset.
-//------------------------------------------------
-//  Functionality:
-//                Q Output data of flip flop.
-//                QB inverted output of Q.
-//                data data input.
-//                clk clock input.
-//                rst reset input.
-//                If reset is enabled the flip flop will reset independent of the clock (Asynchronous).
-//------------------------------------------------
-//  Design Architecture = Behavioural Architecture
-//------------------------------------------------
-//  Module Name   : dFF_AS;
-//  Designer      : Raghav Rastogi;
-//  Creation Date : 28, August, 2018;
-//------------------------------------------------
+/*
+------------------------------------------------
+D Flip Flop with synchronous reset.
+------------------------------------------------
+Functionality:
+				Q Output data of flip flop.
+               	QB inverted output of Q.
+               	data data input.
+               	clk clock input.
+               	rst reset input.
+               	If reset is enabled the flip flop will reset independent of the clock (Asynchronous).
+------------------------------------------------
+Design Architecture = Behavioural Architecture
+------------------------------------------------
+Module Name   : dFF_AS;
+Designer      : Raghav Rastogi;
+Creation Date : 28, August, 2018;
+------------------------------------------------
+*/
 
 module dFF_AS (Q, QB, data, clk, rst);
   
